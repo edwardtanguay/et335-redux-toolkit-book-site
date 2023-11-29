68c8950b6bae302f6c21755576ebc70332c84f49
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-29 13:18:28 +0100

    showing books

--- src/common/pages/PageBooks.tsx
+++ src/common/pages/PageBooks.tsx
@@ -5,10 +5,21 @@ export const PageBooks = () => {
 	const { books } = useSelector((state: RootState) => state.cart);
 	return (
 		<>
-			<p className="mb-4">This is the books page, there are {books.length} books.</p>
-			{books.map((book) => {
-				return <p>{book.title}</p>;
-			})}
+			<p className="mb-4">
+				This is the books page, there are {books.length} books.
+			</p>
+			<div className="flex gap-3 flex-wrap">
+				{books.map((book) => {
+					return (
+						<div>
+							<img
+								className="w-32 h-fit"
+								src={`https://edwardtanguay.vercel.app/share/images/books/${book.idcode}.png`}
+							/>
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,5 @@
 export interface IBook {
 	id: number;
-	idCode: string;
+	idcode: string;
 	title: string;
 }
