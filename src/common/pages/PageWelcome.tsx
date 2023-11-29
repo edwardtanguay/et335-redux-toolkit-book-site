f63aff8fd01e1be03dc0251c3186068b3e1aa307
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-29 10:28:11 +0100

    add/subtract works

--- src/common/pages/PageWelcome.tsx
+++ src/common/pages/PageWelcome.tsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
+import { add, subtract } from "../../features/cart/cartSlice";
 
 export const PageWelcome = () => {
 	const dispatch = useDispatch();
@@ -7,8 +8,8 @@ export const PageWelcome = () => {
 
 	return (
 		<div className="flex gap-3 items-center">
-			<button>Subtract</button>
-			<button>Add</button>
+			<button onClick={() => dispatch(subtract())}>Subtract</button>
+			<button onClick={() => dispatch(add())}>Add</button>
 			<p className="text-xl">count = {count}</p>
 		</div>
 	)
