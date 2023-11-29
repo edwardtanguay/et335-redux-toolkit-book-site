2abdc703434be8eea88be4b1b37d9eeebe924e91
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-29 10:26:06 +0100

    tailwind buttons look good

--- src/common/pages/PageWelcome.tsx
+++ src/common/pages/PageWelcome.tsx
@@ -1,5 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+
 export const PageWelcome = () => {
+	const dispatch = useDispatch();
+	const count = useSelector((state: RootState) => state.cart.count);
+
 	return (
-		<p>This is the welcome page.</p>
+		<div className="flex gap-3 items-center">
+			<button>Subtract</button>
+			<button>Add</button>
+			<p className="text-xl">count = {count}</p>
+		</div>
 	)
 }
